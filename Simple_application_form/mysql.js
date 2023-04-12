bc37de67147f5eb2096a3087151da0998f99e2bc
Gauri Mahashabde <gauri2interact@gmail.com>
2023-04-12 20:08:08 +0530

    Record added

--- Simple_application_form/mysql.js
+++ Simple_application_form/mysql.js
@@ -3,38 +3,54 @@ var url = require("url");
 var mysql = require("mysql");
 
 var server = http.createServer(function (req, res) {
-  console.log("request from client received.....");
-  console.log("processing the request parameters...");
-  const MyURL = new URL(req.url, "http://localhost:8080");
-  var name = MyURL.searchParams.get("Name");
-  var age = MyURL.searchParams.get("Age");
-  var address = MyURL.searchParams.get("Address");
+    console.log("request from client received.....");
+    console.log("processing the request parameters...");
+    const MyURL = new URL(req.url, "http://localhost:8080");
+    var name = MyURL.searchParams.get("Name");
+    var age = MyURL.searchParams.get("Age");
+    var address = MyURL.searchParams.get("Address");
+  
+    console.log("Name = " + name);
+    console.log("Age = " + age);
+    console.log("address = " + address);
+  
+    var con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "Gauri@1029",
+      database: "student_form",
+    });
+  
+    console.log("trying to connet");
+  
+    con.connect(function (err) {
+      if (err) {
+        console.log("error in connection");
+        res.write("Databases connection failures.Try again later");
+        res.end();
+      } 
+      else{
+        console.log("connected successfully");
+        var query = "INSERT INTO students(name , age,address) VALUES(?,?,?)";
+        var values =[Name,Age,Address];
+        con.query(query,values,function(error,result){
+            if(err){
+                console.log("erroe in query");
+                res.write("Internal error occurred");
+                res.end();
+            }
+            else{
+                console.log("Record Inserted");
+                res.write("Record inserted successfully");
+                res.end();
+            }
+        })
 
-  console.log("Name = " + name);
-  console.log("Age = " + age);
-  console.log("address = " + address);
+      }
+      
+    })
+  })
 
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Gauri@1029",
-    database: "student_form",
-  });
-
-  console.log("trying to connet");
-
-  con.connect(function (err) {
-    if (err) {
-      console.log("error in connection");
-      res.write("Databases connection failures.Try again later");
-      res.end();
-    } else {
-      console.log("Record inserted");
-      res.write("Record inserted successfully");
-      res.end();
-    }
-  });
-});
 
 console.log("starting the server.....");
 server.listen(8080);
